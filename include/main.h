505d31502937758c99d0c3c356db3dd8032cccf1
Gabriel Belisario <gabrielbelisariof@gmail.com>
2023-06-17 14:28:00 -0400

    sdl functions and more setup

---
+++ include/main.h
@@ -0,0 +1,29 @@
+#define SDL_MAIN_HANDLED
+#include "imgui.h"
+#include "backends/imgui_impl_sdl2.h"
+#include "backends/imgui_impl_sdlrenderer2.h"
+#include <stdio.h>
+#include "SDL.h"
+
+#include "glm/vec3.hpp"
+#include "glm/vec2.hpp"
+
+#if !SDL_VERSION_ATLEAST(2,0,17)
+#error This backend requires SDL 2.0.17+ because of SDL_RenderGeometry() function
+#endif
+
+// ImGUI functions
+void initImGUI(SDL_Window* window, SDL_Renderer* renderer);
+void drawImGUI();
+void renderImGUI(SDL_Renderer* renderer);
+void cleanImGUI();
+
+// SDL functions
+int SDLInit();
+void SDLClean(SDL_Window* window, SDL_Renderer* renderer);
+
+// input
+void eventHandler(SDL_Window* window, SDL_Renderer* renderer);
+
+// brush
+void putpixel(SDL_Renderer* renderer, glm::vec2 position, glm::vec3 color);
--- main.cpp
+++ main.cpp
@@ -1,32 +1,4 @@
-// Dear ImGui: standalone example application for SDL2 + SDL_Renderer
-// (SDL is a cross-platform general purpose library for handling windows, inputs, OpenGL/Vulkan/Metal graphics context creation, etc.)
-// If you are new to Dear ImGui, read documentation from the docs/ folder + read the top of imgui.cpp.
-// Read online: https://github.com/ocornut/imgui/tree/master/docs
-
-// Important to understand: SDL_Renderer is an _optional_ component of SDL2.
-// For a multi-platform app consider using e.g. SDL+DirectX on Windows and SDL+OpenGL on Linux/OSX.
-#define SDL_MAIN_HANDLED
-#include "imgui.h"
-#include "backends/imgui_impl_sdl2.h"
-#include "backends/imgui_impl_sdlrenderer2.h"
-#include <stdio.h>
-#include "SDL.h"
-
-#include "glm/vec3.hpp"
-#include "glm/vec2.hpp"
-
-#if !SDL_VERSION_ATLEAST(2,0,17)
-#error This backend requires SDL 2.0.17+ because of SDL_RenderGeometry() function
-#endif
-
-void initImGUI(SDL_Window* window, SDL_Renderer* renderer);
-void drawImGUI();
-void renderImGUI(SDL_Renderer* renderer);
-void cleanImGUI();
-
-void eventHandler(SDL_Window* window, SDL_Renderer* renderer);
-
-void putpixel(SDL_Renderer* renderer, glm::vec2 position, glm::vec3 color);
+#include "main.h"
 
 // Our state
 bool show_demo_window = true;
@@ -34,35 +6,17 @@ bool show_another_window = false;
 ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 bool done = false;
 
+// SDL things
+SDL_WindowFlags window_flags;
+SDL_Window* window;
+SDL_Renderer* renderer;
+
 // Main code
 int main(int, char**){
-    // Setup SDL
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0){
-        printf("Error: %s\n", SDL_GetError());
-        return -1;
-    }
-
-    // From 2.0.18: Enable native IME.
-#ifdef SDL_HINT_IME_SHOW_UI
-    SDL_SetHint(SDL_HINT_IME_SHOW_UI, "1");
-#endif
-
-    // Create window with SDL_Renderer graphics context
-    SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
-    SDL_Window* window = SDL_CreateWindow("Dear ImGui SDL2+SDL_Renderer example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, window_flags);
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
-    if (renderer == nullptr)
-    {
-        SDL_Log("Error creating SDL_Renderer!");
-        return 0;
-    }
-    //SDL_RendererInfo info;
-    //SDL_GetRendererInfo(renderer, &info);
-    //SDL_Log("Current SDL_Renderer: %s", info.name);
-
-
+    
+    if(SDLInit() != 0)
+        return 1;
     initImGUI(window, renderer);
-    SDL_Surface *surface;
 
     // Main loop
     while (!done){
@@ -76,6 +30,7 @@ int main(int, char**){
             for (size_t j = 0; j < 50; j++)
                 putpixel(renderer, glm::vec2(i,j), glm::vec3(255, 0, 0));
         
+        
         drawImGUI();
         renderImGUI(renderer);
 
@@ -84,10 +39,41 @@ int main(int, char**){
     }
 
     cleanImGUI();
+    SDLClean(window, renderer);
 
+    return 0;
+}
+
+void SDLClean(SDL_Window* window, SDL_Renderer* renderer){
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
+}
+
+int SDLInit(){
+    // Setup SDL
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0){
+        printf("Error: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    // From 2.0.18: Enable native IME.
+    #ifdef SDL_HINT_IME_SHOW_UI
+        SDL_SetHint(SDL_HINT_IME_SHOW_UI, "1");
+    #endif
+
+    // Create window with SDL_Renderer graphics context
+    window_flags = (SDL_WindowFlags)(SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
+    window = SDL_CreateWindow("Dear ImGui SDL2+SDL_Renderer example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, window_flags);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
+    if (renderer == nullptr)
+    {
+        SDL_Log("Error creating SDL_Renderer!");
+        return 1;
+    }
+    //SDL_RendererInfo info;
+    //SDL_GetRendererInfo(renderer, &info);
+    //SDL_Log("Current SDL_Renderer: %s", info.name);
 
     return 0;
 }
